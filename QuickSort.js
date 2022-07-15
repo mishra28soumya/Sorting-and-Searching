9e3d9d070aa728dbfa7e805afad3836ec209f6b7
mishra28soumya <mishra28soumya@gmail.com>
2022-07-15 17:41:00 +0530

    Create QuickSort.js

---
+++ QuickSort.js
@@ -0,0 +1,51 @@
+/** Element which needs to be placed in the correct position is called pivot */
+function quickSort(arr, s, e){
+    if(s>=e){
+        return;
+    }
+    let p = partition(arr,s,e);
+    quickSort(arr,s,p-1);
+    quickSort(arr,p+1,e);
+}
+
+function swap(a,b){
+    let t = a;
+    a = b;
+    b = t;
+
+}
+
+function partition(arr, s, e){
+    let pivot = arr[s];
+    let c = 0;
+    for(let i=s+1; i<=e; i++){
+        if(arr[i]<=pivot){
+            c++;
+        }
+    }
+    let pivotIndex = s + c;
+    let t = arr[pivotIndex];
+    arr[pivotIndex] = arr[s];
+    arr[s] = t;
+
+    let i=s, j=e;
+    while(i<pivotIndex && j>pivotIndex){
+        while(arr[i]<=pivot){
+            i++;
+        }
+        while(arr[j]>=pivot){
+            j--;
+        }
+        if(i<pivotIndex && j>pivotIndex){
+            let t= arr[i];
+            arr[i] = arr[j];
+            arr[j]=t;
+        }
+    }
+    return pivotIndex;
+
+}
+
+arr = [5,4,1,2,3]
+quickSort(arr, 0, 4)
+console.log(arr)

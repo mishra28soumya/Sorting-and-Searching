5c5397f578d63896db0fa1327343c1df45879d0f
mishra28soumya <mishra28soumya@gmail.com>
2022-08-03 16:49:01 +0530

    Create LinearSearch.js

---
+++ LinearSearch.js
@@ -0,0 +1,17 @@
+
+function linearSearch(value, list) {
+    let found = false;
+    let position = -1;
+    let index = 0;
+  
+    while(!found && index < list.length) {
+        if(list[index] == value) {
+            found = true;
+            position = index;
+        } else {
+            index += 1;
+        }
+    }
+    return position;
+}
+

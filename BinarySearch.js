73266ac711578f55eb01246562459619a94e5a3e
mishra28soumya <mishra28soumya@gmail.com>
2022-08-05 12:33:46 +0530

    Update BinarySearch.js

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,14 +1,31 @@
 
-binarySearch(arr, x, low, high)
-    if low > high
-        return False 
-    else
-        mid = (low + high) / 2 
-        if x == arr[mid]
-            return mid
-        else if x > arr[mid]    
-            return binarySearch(arr, x, mid + 1, high)
-        else                              
-            return binarySearch(arr, x, low, mid - 1)
+function binarySearch (arr, x, start, end) {
+	
+	// Base Condition
+	if (start > end) return false;
+
+	// Find the middle index
+	let mid=Math.floor((start + end)/2);
+
+	// Compare mid with given key x
+	if (arr[mid]===x) return true;
+		
+	// If element at mid is greater than x,
+	// search in the left half of mid
+	if(arr[mid] > x)
+		return binarySearch(arr, x, start, mid-1);
+	else
+
+		// If element at mid is smaller than x,
+		// search in the right half of mid
+		return binarySearch(arr, x, mid+1, end);
+}
+
+let arr = [1, 3, 5, 7, 8, 9];
+let x = 5;
+
+console.log(binarySearch(arr,x,0,arr.length-1));
+								
+
             
             
